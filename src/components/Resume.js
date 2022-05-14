a335a6440c45842f12cb0bbe8f17d0a71963e2f3
FraustoB <brandon.frausto18@gmail.com>
2022-05-13 21:05:05 -0500

    added icons and setup links for Resume, Github, and LinkedIn connections

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,43 @@
 import "../styles/Resume.scss";
+import { BiSpreadsheet } from "react-icons/bi";
+import { FaLinkedin, FaGithubAlt } from "react-icons/fa";
 
 const Resume = () => {
   return (
-    <div id="Resume">
-      <main>Resume</main>
+    <div id="Resume" className="Resume-container">
+      <main className="Resume-main">
+        <h1>Important Links!</h1>
+        <div className="Icons-container">
+          <BiSpreadsheet size={75} />
+          <a
+            href="https://docs.google.com/document/d/1oioq11blSa_0rQY0p_s70JdyeM1fAd2SAdv_RqAn6II/edit?usp=sharing"
+            target="_blank"
+            className="Resume-links"
+          >
+            RESUME LINK
+          </a>
+        </div>
+        <div className="Icons-container">
+          <FaLinkedin size={75} />
+          <a
+            href="https://docs.google.com/document/d/1oioq11blSa_0rQY0p_s70JdyeM1fAd2SAdv_RqAn6II/edit?usp=sharing"
+            target="_blank"
+            className="Resume-links"
+          >
+            LinkedIn
+          </a>
+        </div>
+        <div className="Icons-container">
+          <FaGithubAlt size={75} />
+          <a
+            href="https://docs.google.com/document/d/1oioq11blSa_0rQY0p_s70JdyeM1fAd2SAdv_RqAn6II/edit?usp=sharing"
+            target="_blank"
+            className="Resume-links"
+          >
+            GitHub
+          </a>
+        </div>
+      </main>
     </div>
   );
 };
