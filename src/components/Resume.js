3955a8a77b31704675275ec9866f94a84a0010fe
FraustoB <brandon.frausto18@gmail.com>
2022-05-27 19:59:28 -0500

    changed div to anchor and anchor to div in the contact page to allow for full icon and link webpage push

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,36 +8,32 @@ const Resume = () => {
       <main className="Resume-main">
         <h1>Connect With Me</h1>
         <div className="Resume-grid">
-          <div className="Icons-container">
-            <BiSpreadsheet size={75} />
-            <a
-              href="https://docs.google.com/document/d/1oioq11blSa_0rQY0p_s70JdyeM1fAd2SAdv_RqAn6II/edit?usp=sharing"
-              target="_blank"
-              className="Resume-links"
-            >
-              RESUME
-            </a>
-          </div>
-          <div className="Icons-container">
-            <FaLinkedin size={75} />
-            <a
-              href="https://www.linkedin.com/in/brandon-s-frausto-13a712213/"
-              target="_blank"
-              className="Resume-links"
-            >
-              LinkedIn
-            </a>
-          </div>
-          <div className="Icons-container">
-            <FaGithubAlt size={75} />
-            <a
-              href="https://github.com/FraustoB"
-              target="_blank"
-              className="Resume-links"
-            >
-              GitHub
-            </a>
-          </div>
+          <a
+            href="https://docs.google.com/document/d/1oioq11blSa_0rQY0p_s70JdyeM1fAd2SAdv_RqAn6II/edit?usp=sharing"
+            target="_blank"
+            className="Icons-container"
+          >
+            <BiSpreadsheet size={100} />
+            <div className="Resume-links">RESUME</div>
+          </a>
+
+          <a
+            className="Icons-container"
+            href="https://www.linkedin.com/in/brandon-s-frausto-13a712213/"
+            target="_blank"
+          >
+            <FaLinkedin size={100} />
+            <div className="Resume-links">LinkedIn</div>
+          </a>
+
+          <a
+            className="Icons-container"
+            href="https://github.com/FraustoB"
+            target="_blank"
+          >
+            <FaGithubAlt size={100} />
+            <div className="Resume-links">GitHub</div>
+          </a>
         </div>
         <footer>
           <a href="frausto.dev">frausto.dev</a>
